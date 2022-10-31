3c256de1c245bdea36d26b86f3ff8333ede92747
Ashutosh <ashutosh.na.99@gmail.com>
2022-10-31 21:45:13 +0530

    Version 2: Dynamic Stuff

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,33 @@
 
 use bevy::prelude::*;
 
+// region: --- Asset Constants
 
+const PLAYER_SPRITE: &str = "player_a_01.png";
+const PLAYER_SIZE: (f32, f32) = (144., 75.);
+
+const SPRITE_SCALE: f32 = 0.5;
+
+// endregion: --- Asset Constants
+
+
+// region: --- Resources
+
+pub struct WinSize {
+    pub w: f32,
+    pub h: f32,
+}
+
+// endregion: --- Resources
 
 
 fn main() {
     App::new()
-        .insert_resource(ClearColor(Color::rgb(0.04,0.04,0.04)))
+        .insert_resource(ClearColor(Color::rgb(0.04, 0.04, 0.04)))
         .insert_resource(WindowDescriptor {
             title: "Rust Invaders".to_string(),
-            width: 598.0,
-            height:676.0,
+            width: 400.0,
+            height: 400.0,
             ..Default::default()
         })
         .add_plugins(DefaultPlugins)
@@ -19,18 +36,36 @@ fn main() {
         .run();
 }
 
+fn setup_system(
+    mut commands: Commands, 
+    asset_server: Res<AssetServer>,
+    mut windows: ResMut<Windows>,
+) {
 
-fn setup_system(mut commands: Commands) {
     // Add a camera
     commands.spawn_bundle(Camera2dBundle::default());
 
-    // Add a rectangle
+    // capture window size
+    let window = windows.get_primary_mut().unwrap();
+    let(win_w, win_h) = (window.width(), window.height());
+
+    // position window
+    window.set_position(IVec2::new(2780, 4900));
+
+    let win_size = WinSize{w: win_w, h: win_h};
+
+    // Add a player 
+    let bottom = -win_h / 2.;
+
     commands.spawn_bundle(SpriteBundle {
-        sprite: Sprite {
-            color: Color::rgb(0.25, 0.25, 0.25),
-            custom_size: Some(Vec2::new(150.,150.)),
+        texture: asset_server.load(PLAYER_SPRITE),
+        transform: Transform {
+            translation: Vec3::new(0., bottom + PLAYER_SIZE.1 / 2. * SPRITE_SCALE + 5., 10.),
+            scale: Vec3::new(SPRITE_SCALE, SPRITE_SCALE, 1.),
             ..Default::default()
         },
         ..Default::default()
     });
 }
+
+
