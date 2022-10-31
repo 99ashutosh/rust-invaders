22a1cc2153589e36f68a2929d934e6f652562f86
Ashutosh <ashutosh.na.99@gmail.com>
2022-10-31 21:20:59 +0530

    Basic Example

---
+++ src/main.rs
@@ -0,0 +1,36 @@
+#![allow(unused)]
+
+use bevy::prelude::*;
+
+
+
+
+fn main() {
+    App::new()
+        .insert_resource(ClearColor(Color::rgb(0.04,0.04,0.04)))
+        .insert_resource(WindowDescriptor {
+            title: "Rust Invaders".to_string(),
+            width: 598.0,
+            height:676.0,
+            ..Default::default()
+        })
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(setup_system)
+        .run();
+}
+
+
+fn setup_system(mut commands: Commands) {
+    // Add a camera
+    commands.spawn_bundle(Camera2dBundle::default());
+
+    // Add a rectangle
+    commands.spawn_bundle(SpriteBundle {
+        sprite: Sprite {
+            color: Color::rgb(0.25, 0.25, 0.25),
+            custom_size: Some(Vec2::new(150.,150.)),
+            ..Default::default()
+        },
+        ..Default::default()
+    });
+}
