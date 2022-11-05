a12cae0b318e12d0b654810616307b2bceecd163
Ashutosh <ashutosh.na.99@gmail.com>
2022-11-05 09:17:01 +0530

    Added enemy fire system

--- src/enemy.rs
+++ src/enemy.rs
@@ -1,8 +1,10 @@
-use bevy::{prelude::*, time::FixedTimestep};
+use std::f32::consts::PI;
+
+use bevy::{prelude::*, time::FixedTimestep, ecs::schedule::ShouldRun};
 use rand::{thread_rng, Rng};
 
 use crate::{
-    components::{Enemy, SpriteSize},
+    components::{Enemy, SpriteSize, Movable, Laser, FromEnemy, Velocity},
     EnemyCount, GameTextures, WinSize, ENEMY_LASER_SIZE, ENEMY_MAX, ENEMY_SIZE, PLAYER_SIZE,
     SPRITE_SCALE,
 };
@@ -15,7 +17,13 @@ impl Plugin for EnemyPlugin {
             SystemSet::new()
                 .with_run_criteria(FixedTimestep::step(1.))
                 .with_system(enemy_spawn_system),
+        )
+        .add_system_set(
+            SystemSet::new()
+            .with_run_criteria(enemy_fire_criteria)
+            .with_system(enemy_fire_system),
         );
+
     }
 }
 
@@ -46,5 +54,39 @@ fn enemy_spawn_system(
             .insert(SpriteSize::from(ENEMY_SIZE));
 
         enemy_count.0 += 1;
+    }
+}
+
+fn enemy_fire_criteria() -> ShouldRun {
+    if thread_rng().gen_bool(1./60.) {
+        ShouldRun::Yes
+    } else {
+        ShouldRun::No
+    }
+}
+
+fn enemy_fire_system (
+    mut commands: Commands,
+    game_textures: Res<GameTextures>,
+    enemy_query: Query<&Transform, With<Enemy>>,
+) {
+    for &tf in enemy_query.iter() {
+        let (x,y) = (tf.translation.x, tf.translation.y);
+        commands
+        .spawn_bundle(SpriteBundle {
+            texture: game_textures.enemy_laser.clone(),
+            transform: Transform {
+                translation: Vec3::new(x, y - 15., 0.),
+                scale: Vec3::new(SPRITE_SCALE, SPRITE_SCALE, 1.),
+                rotation: Quat::from_rotation_x(PI),
+                ..Default::default()
+            },
+            ..Default::default()
+        })
+        .insert(Laser)
+        .insert(SpriteSize::from(ENEMY_LASER_SIZE))
+        .insert(FromEnemy)
+        .insert(Movable {auto_despawn: true})
+        .insert(Velocity {x: 0., y: -1.});
     }
 }
